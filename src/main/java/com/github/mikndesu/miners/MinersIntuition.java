3c14f373c4c25cfc6b21c25823bd8cd8168f9643
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-07-11 01:25:45 +0900

    set up workspace for project

---
+++ src/main/java/com/github/mikndesu/miners/MinersIntuition.java
@@ -0,0 +1,17 @@
+package com.github.mikndesu.miners;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MinersIntuition implements ModInitializer {
+
+    public static final String MOD_ID = "miners_intuition";
+    public static final Logger LOGGER = LogManager.getLogger("MinersIntuition/Main");
+
+    @Override
+    public void onInitialize() {
+    }
+    
+}
