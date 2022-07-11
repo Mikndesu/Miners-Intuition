dbe9f816cd9b7b9587f5e29f2799c9f8b8b19568
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-07-11 18:40:37 +0900

    make this mod a client side mod

--- src/main/java/com/github/mikndesu/miners/MinersIntuition.java
+++ src/main/java/com/github/mikndesu/miners/MinersIntuition.java
@@ -3,15 +3,14 @@ package com.github.mikndesu.miners;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
-public class MinersIntuition implements ModInitializer {
+public class MinersIntuition implements ClientModInitializer {
 
     public static final String MOD_ID = "miners_intuition";
     public static final Logger LOGGER = LogManager.getLogger("MinersIntuition/Main");
-
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {  
     }
     
 }
