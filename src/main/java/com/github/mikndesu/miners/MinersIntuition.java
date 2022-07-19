29d0f3b18e803a3682d51f4b0857e8c67a655aa0
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2022-07-20 01:57:35 +0900

    add Config for target blocks

--- src/main/java/com/github/mikndesu/miners/MinersIntuition.java
+++ src/main/java/com/github/mikndesu/miners/MinersIntuition.java
@@ -1,16 +1,40 @@
 package com.github.mikndesu.miners;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.github.mikndesu.miners.config.IntuitionConfig;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.ResourceLocationException;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class MinersIntuition implements ClientModInitializer {
 
     public static final String MOD_ID = "miners_intuition";
     public static final Logger LOGGER = LogManager.getLogger("MinersIntuition/Main");
+    public static List<BlockState> acceptedBlocks = new ArrayList<>();
+
     @Override
-    public void onInitializeClient() {  
+    public void onInitializeClient() {
+        AutoConfig.register(IntuitionConfig.class, JanksonConfigSerializer::new);
+        var config = AutoConfig.getConfigHolder(IntuitionConfig.class).getConfig();
+        for(var blockRegistryName:config.registryNameList) {
+            try {
+                var resourceLocation = new ResourceLocation(blockRegistryName);
+                var block = Registry.BLOCK.get(resourceLocation);
+                acceptedBlocks.add(block.defaultBlockState());
+            } catch(ResourceLocationException e) {
+                continue;
+            }
+        }  
     }
     
 }
--- src/main/java/com/github/mikndesu/miners/config/IntuitionConfig.java
+++ src/main/java/com/github/mikndesu/miners/config/IntuitionConfig.java
@@ -1,6 +1,6 @@
 package com.github.mikndesu.miners.config;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.github.mikndesu.miners.MinersIntuition;
@@ -14,5 +14,5 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 public class IntuitionConfig implements ConfigData {
     @RequiresRestart
     @Comment("Changes will be applied to your client after restarting.")
-    public List<String> registryNameList = new ArrayList<>();
+    public List<String> registryNameList = Arrays.asList("minecraft:diamond_ore", "minecraft:gold_ore", "minecraft:iron_ore", "minecraft:deepslate_diamond_ore", "minecraft:deepslate_gold_ore", "minecraft:deepslate_iron_ore");
 }
