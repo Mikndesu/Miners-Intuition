96dfcb1b8bae24707f570320c0cd37c3aac118cf
Mitsuki Goto <48896872+MitsukiGoto@users.noreply.github.com>
2022-07-25 01:27:02 +0900

    add caution system if lava is close to player

--- src/main/java/com/github/mikndesu/miners/asm/mixin/GuiMixin.java
+++ src/main/java/com/github/mikndesu/miners/asm/mixin/GuiMixin.java
@@ -33,16 +33,21 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Gui.class)
 @Environment(EnvType.CLIENT)
 public class GuiMixin {
-    @Inject(method = "renderPlayerHealth", at=@At(value="INVOKE", target="Lnet/minecraft/util/profiling/ProfilerFiller;pop()V"))
+    @Inject(method = "renderPlayerHealth", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiling/ProfilerFiller;pop()V"))
     public void inject(PoseStack poseStack, CallbackInfo ci) {
         Minecraft client = Minecraft.getInstance();
+        LocalPlayer player = client.player;
         final Font textRenderer = client.font;
         final ItemRenderer itemRenderer = client.getItemRenderer();
         RenderSystem.enableBlend();
@@ -54,18 +59,33 @@ public class GuiMixin {
         modelMatrices.scale(1.0f, 1.0f, 1.0f);
         RenderSystem.applyModelViewMatrix();
         int i = 0;
-        for(var element:IntuitionResult.getInstance().getResult()) {
-            if(element.getValue() != 0) {
-                int baseX = 0;
-                int baseY = i * 20;
-                String text = element.getValue() > 30 ? "30+": String.valueOf(element.getValue());
-                textRenderer.draw(poseStack, "\u00d7 " + text, baseX + 20, baseY+5, 0xFFFFFF);
-                itemRenderer.renderGuiItem(new ItemStack(element.getKey().getBlock().asItem()), baseX, baseY);
-                i++;
+        for (var element : IntuitionResult.getInstance().getResult()) {
+            int baseX = 0;
+            int baseY = i * 20;
+            if (element.getValue() != 0) {
+                if (element.getKey() == Blocks.LAVA.defaultBlockState() && element.getValue()>=5 && isLavaCautionAvailable(player)) {
+                    String text = "Caution: Lava Lake may be near by!";
+                    textRenderer.draw(poseStack, text, (client.getWindow().getGuiScaledWidth()-client.font.width(text))/2, baseY + 5, 0xDC143C);
+                } else {
+                    String text = element.getValue() > 30 ? "30+" : String.valueOf(element.getValue());
+                    textRenderer.draw(poseStack, "\u00d7 " + text, baseX + 20, baseY + 5, 0xFFFFFF);
+                    itemRenderer.renderGuiItem(new ItemStack(element.getKey().getBlock().asItem()), baseX, baseY);
+                    i++;
+                }
             }
         }
         modelMatrices.popPose();
         modelMatrices.popPose();
         RenderSystem.applyModelViewMatrix();
+    }
+
+    public boolean isLavaCautionAvailable(LocalPlayer player) {
+        var dimension = player.clientLevel.dimension();
+        if(dimension == Level.NETHER) {
+            return false;
+        } else if(dimension == Level.OVERWORLD) {
+            return player.position().y < 0;
+        }
+        return true;
     }
 }
